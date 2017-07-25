36fa99208ce2c2050f3e3e7b913faae52376c35d
dana19-meet <dana19@meet.mit.edu>
2017-07-25 15:46:33 +0200

    Stopping here

---
+++ hello  world.py
@@ -0,0 +1,66 @@
+Python 3.5.2 (default, Nov 17 2016, 17:05:23) 
+[GCC 5.4.0 20160609] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> print("hello world")
+hello world
+>>> print('hello world')
+hello world
+>>> print('hello simon')
+hello simon
+>>> #print('hello eorld')
+>>> 
+======= RESTART: /home/meet/dana19_lab3/meet2017y1lab3/hello world.py =======
+你好，世界 is how you say hello world in chinese
+>>> twoRoads = "Two roads diverged in a wood, and I-"
+roadTaken = "I took the one most traveled by,"
+less = "I took the one less traveled by,"
+theDifference = "And that has made all the difference."
+roadTaken = less
+print(twoRoads)
+print(roadTaken)
+print(theDifference)
+SyntaxError: multiple statements found while compiling a single statement
+>>> 
+======= RESTART: /home/meet/dana19_lab3/meet2017y1lab3/RobertFrost.py =======
+Two roads diverged in a wood, and I-
+I took the one less traveled by,
+And that has made all the difference.
+>>> 
+======= RESTART: /home/meet/dana19_lab3/meet2017y1lab3/RobertFrost.py =======
+Two roads diverged in a wood, and I-
+I took the one less traveled by,
+And that has made all the difference.
+>>> twoRoads = "Two roads diverged in a wood, and I-"
+#roadTaken = "I took the one most traveled by,"
+less = "I took the one less traveled by,"
+theDifference = "And that has made all the difference."
+roadTaken = less
+print(twoRoads)
+print(roadTaken)
+print(theDifference)
+SyntaxError: multiple statements found while compiling a single statement
+>>> twoRoads = "Two roads diverged in a wood, and I-"
+#roadTaken = "I took the one most traveled by,"
+less = "I took the one less traveled by,"
+theDifference = "And that has made all the difference."
+roadTaken = less
+print(twoRoads)
+print(roadTaken)
+print(theDifference)
+SyntaxError: multiple statements found while compiling a single statement
+>>> 
+======= RESTART: /home/meet/dana19_lab3/meet2017y1lab3/RobertFrost.py =======
+Two roads diverged in a wood, and I-
+I took the one less traveled by,
+And that has made all the difference.
+>>> 
+======= RESTART: /home/meet/dana19_lab3/meet2017y1lab3/RobertFrost.py =======
+Two roads diverged in a wood, and I-
+I took the one less traveled by,
+And that has made all the difference.
+>>> 
+======= RESTART: /home/meet/dana19_lab3/meet2017y1lab3/RobertFrost.py =======
+Two roads diverged in a wood, and I-
+I took the one most traveled by,
+And that has made all the difference.
+>>> 
---
+++ hello world.py
@@ -0,0 +1,1 @@
+print("你好，世界 is how you say hello world in chinese")
